37cb608a997c359094a7abe5e64deb221488bc68
Carlos Primo <carlosprimob@gmail.com>
2022-02-01 08:55:01 -0500

    first commit

---
+++ fashion_like/src/main/java/un/caprimo/fashion_like/FashionLikeApplication.java
@@ -0,0 +1,13 @@
+package un.caprimo.fashion_like;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionLikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FashionLikeApplication.class, args);
+	}
+
+}
---
+++ fashion_like/src/test/java/un/caprimo/fashion_like/FashionLikeApplicationTests.java
@@ -0,0 +1,13 @@
+package un.caprimo.fashion_like;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FashionLikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
